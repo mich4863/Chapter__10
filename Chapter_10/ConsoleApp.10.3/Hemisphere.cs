2b1daa95ec24c1f957bb703ef1fdf8117d76d817
Michael Overgaard Nielsen <mich4863@edu.campusvejle.dk>
2018-08-16 14:58:58 +0200

    ...

---
+++ Chapter_10/ConsoleApp.10.3/Hemisphere.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp._10._3
+{
+    class Hemisphere
+    {
+        private decimal radius;
+
+        public Hemisphere(decimal radius)
+        {
+            Radius = radius;
+        }
+
+        public decimal Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                if(value > 0.0m && value < 12.0m)
+                {
+                    radius = value;
+                }
+            }
+        }
+
+        public decimal Volume
+        {
+            get;
+        }
+
+        public decimal CurvedSurfaceArea
+        {
+            get;
+        }
+
+        public decimal TotalSurfaceArea
+        {
+            get;
+        }
+    }
+}
---
+++ Chapter_10/ConsoleApp.10.3/HemisphereTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp._10._3
+{
+    class HemisphereTest
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
