4a4ee27a6c48cbdd1cbae8f2131d0637e2e7704c
Michael Overgaard Nielsen <mich4863@edu.campusvejle.dk>
2018-08-17 11:59:48 +0200

    ...

--- Chapter_10/ConsoleApp.10.4/Time2.cs
+++ Chapter_10/ConsoleApp.10.4/Time2.cs
@@ -6,7 +6,89 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp._10._4
 {
-    class Time2
+    public class Time2
     {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time2(int hour = 0, int minute = 0, int second = 0)
+        {
+            SetTime(hour, minute, second);
+        }
+
+        public Time2(Time2 time)
+            : this(time.Hour, time.Minute, time.Second) { }
+
+        public void SetTime(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            
+            /*hour = hour * 3600;
+            minute = minute * 60;
+            int result = hour + minute + second;
+
+            Second = result;*/
+        }
+
+        public int Hour
+        {
+            get
+            {
+                return hour;
+            }
+
+            set
+            {
+                if(value < 0 || value > 23)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Hour)} must be 0-23");
+                }
+                hour = value;
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+
+            set
+            {
+                if(value < 0 || value > 59)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Minute)} must be 0-59");
+                }
+                minute = value;
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+
+            set
+            {
+                if(value < 0 || value > 59)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Second)} must be 0-59");
+                }
+                second = value;
+            }
+        }
+
+        public string ToUniversalString() =>
+            $"{Hour:D2}:{Minute:D2}:{Second:D2}";
+
+        public override string ToString() =>
+            $"{((Hour == 0 || hour == 12) ? 12 : Hour % 12)}:" +
+            $"{Minute:D2}:{Second:D2} {(Hour < 12 ? "AM" : "PM")}";
     }
 }
--- Chapter_10/ConsoleApp.10.4/Time2Test.cs
+++ Chapter_10/ConsoleApp.10.4/Time2Test.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp._10._4
 {
-    class Time2Test
+    public class Time2Test
     {
         static void Main(string[] args)
         {
+            var t1 = new Time2();
+            var t2 = new Time2(2);
+            var t3 = new Time2(21, 34);
+            var t4 = new Time2(12, 25, 42);
+            var t5 = new Time2(t4);
+            
+            Console.WriteLine("Constructed with:\n");
+            Console.WriteLine("t1: all arguments defaulted");
+            Console.WriteLine($"    {t1.ToUniversalString()}");
+            Console.WriteLine($"    {t1.ToString()}");
+
+            Console.WriteLine("t2: hour specified; minute and second defaulted");
+            Console.WriteLine($"    {t2.ToUniversalString()}");
+            Console.WriteLine($"    {t2.ToString()}");
+
+            Console.WriteLine("t3: hour and minute specified; second defaulted");
+            Console.WriteLine($"    {t3.ToUniversalString()}");
+            Console.WriteLine($"    {t3.ToString()}");
+
+            Console.WriteLine("t4: hour, minute and second specified;");
+            Console.WriteLine($"    {t4.ToUniversalString()}");
+            Console.WriteLine($"    {t4.ToString()}");
+
+            Console.WriteLine("t5: Time2 object t4 specified;");
+            Console.WriteLine($"    {t5.ToUniversalString()}");
+            Console.WriteLine($"    {t5.ToString()}");
+
+            try
+            {
+                var t6 = new Time2(27, 74, 99);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("\nException while initializing t6:");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
